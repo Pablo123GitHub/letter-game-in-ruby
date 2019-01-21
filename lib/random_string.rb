1c7f8157a18f2f49c85a981e8aabbbf73d91ae91
Pablo <paul.ladiv123@gmail.com>
2019-01-21 10:28:18 +0000

    show random upper case word with the desired length as argument

---
+++ lib/random_string.rb
@@ -0,0 +1,19 @@
+class RandomString
+    def show_random_word (input_number)
+        alphabet = build_alphabet()
+        response = ""
+        input_number.times do  
+            number = Kernel.rand(26) 
+            letter = alphabet[number-1]
+            response = response + letter
+        end 
+        response 
+    end 
+
+    private
+
+    def build_alphabet
+        ("A".."Z").to_a
+    end 
+
+end 
---
+++ spec/random_string_spec.rb
@@ -0,0 +1,17 @@
+require_relative("../lib/random_string.rb")
+
+describe RandomString do   
+    let(:subject) { described_class.new }
+
+    context "output a random string with X amount of letters" do   
+        it "shows one random letter stubbed as being A" do          
+            allow(Kernel).to receive(:rand).and_return(1)
+            expect(subject.show_random_word(1)).to eq("A")
+        end 
+
+        it "shows two random letters stubbed as being AB" do   
+            allow(Kernel).to receive(:rand).and_return(1,2)
+            expect(subject.show_random_word(2)).to eq("AB")
+        end 
+    end 
+end 
