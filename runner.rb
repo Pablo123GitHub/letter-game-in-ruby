2b143b36767617c4df98bb53694681c852e6b718
Pablo <paul.ladiv123@gmail.com>
2019-01-31 12:29:07 +0000

    update runner to use a real 5 letter random word

--- runner.rb
+++ runner.rb
@@ -1,25 +1,18 @@
 
 require_relative("./lib/game")
-# require_relative("./lib/random_string")
+require_relative("./lib/random_string")
 
-@correct_answer = "bacef"
-
-@game = Game.new(@correct_answer)
-
-hey = "whatever"
+@game = Game.new
 
 def prompt(*args)
     print(*args)
     gets
 end
 
-
 while true
     response = prompt "Input your response: "
     @game.player_response(response)
     assessment = @game.assess_response_right_or_wrong
     puts assessment
-    if @game.player_found_answer
-        break
-    end 
+    break if @game.player_found_answer || @game.player_failed_to_find_answer
 end
